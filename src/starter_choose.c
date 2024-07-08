6256ffde26c3e21e9948d1a543dc556c51897df6
unfoldingdev <113267622+unfoldingdev@users.noreply.github.com>
2024-07-07 17:55:51 -0700

    First Commit
    Might have made a change somewhere, but I don't recall it :(

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -114,7 +114,7 @@ static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
     SPECIES_TREECKO,
     SPECIES_TORCHIC,
-    SPECIES_MUDKIP,
+    SPECIES_DEWPIDER,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
